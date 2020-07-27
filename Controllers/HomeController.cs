ae551518bd0b659bfd9342bf4cb619b624082d1d
Charan Chandra <charanchandrap@gmail.com>
2020-07-27 20:18:07 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Repository/BookRepository.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using BookStore.Models;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        
+    }
+}
