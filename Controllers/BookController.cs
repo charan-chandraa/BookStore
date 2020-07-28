fbd3e63477bb0f513f1d017b04f162203424d25d
Charan Chandra <charanchandrap@gmail.com>
2020-07-28 23:10:13 +0530

    july28

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using BookStore.Repository;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class BookController : Controller
     {
-        public string GetAllBooks()
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
         {
-            return "All Bookss";
+            _bookRepository = new BookRepository();
+        }
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View(data);
+        }
+        [Route("book-details/{id}", Name="bookDetailsRoute")]
+        public ViewResult GetBook(int id)
+        {
+            var book =  _bookRepository.GetBookById(id);
+            return View(book);
+        }
+        public ViewResult SearchBook(string title, string author)
+        {
+            var data = _bookRepository.SearchBook(title, author);
+            return View();
+        }
+        public ViewResult AddNewBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult AddNewBook(BookModel bookModel)
+        {   
+            return View();
         }
     }
 }
--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -1,10 +1,33 @@
 using System.Collections.Generic;
 using BookStore.Models;
+using System.Linq;
 
 namespace BookStore.Repository
 {
     public class BookRepository
     {
-        
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1, Title="The Alchemist", Author="Paulo Coelho", Description="This is the description for The Alchemist book."},
+                new BookModel(){Id=2, Title="The legend of suheldev", Author="Amish Tripati", Description="This is the description for The legend of suheldev book."},
+                new BookModel(){Id=3, Title="Shiva Trilogy", Author="Amish Tripati", Description="This is the description for Shiva Triology book."},
+                new BookModel(){Id=4, Title="The lost Symbol", Author="Dan Brown", Description="This is the description for The lost symbol book."},
+                new BookModel(){Id=5, Title="Stranger Trilogy", Author="Navoneel Chakrabothy", Description="This is the description for Stranger Trilogy book."}
+            };
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id ==id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
     }
 }
